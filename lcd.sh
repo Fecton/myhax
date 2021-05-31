75278ee86e97356555b6840258a5774be59f66c4
Daniel <76391547+Fecton@users.noreply.github.com>
2021-05-31 11:01:31 +0300

    Update lcd.sh

--- lcd.sh
+++ lcd.sh
@@ -1,57 +1,5 @@
 #!/usr/bin/env bash
 
-function install_necessary(){
-	sudo apt -y install git
-}
-
-function show_list(){
-	echo "0. Show menu"
-	echo "1. Downdload driver"
-	echo "2. Installing/starting with argument - degresses (default 180)"
-	echo "3. Return to hdmi-mode"
-	echo "4. Exit"
-	echo "--- ---"
-}
-
-function get_driver(){
-	install_necessary
-	cd ~
-	if ! [[ -d tools  ]]; then
-		mkdir tools
-		cd tools
-	fi
-
-	if [[ $(ls)  ]]; then
-		echo "Hello"
-	fi
-
-	git clone https://github.com/waveshare/LCD-show.git
-	chmod +x LCD-show/LCD28-show
-	echo "[+] Successfully downloaded!"
-	echo "--- ---"
-}
-
-function driver_start(){
-	cd ~/tools/LCD-show/
-	echo "[?] Are you sure? y/n"
-	read yes_or_no
-	if [[ $yes_or_no == "yes" ]];then
-		echo "[?] Degreeses:"
-		read degreeses
-		./LCD28-show $degreeses
-		echo "[+] Bam-bam! Reload!"
-	else
-		echo "[-] Exitting!"
-		exit
-	fi
-}
-
-function driver_stop(){
-	cd ~/tools/LCD-show/
-	./LCD-hdmi
-	echo "[+] Tyk-tyk! Reload!"
-}
-
 if [[ $(uname -o) != "GNU/Linux" ]]; then
 	echo "[-] Supported only on GNU/Linux!"
 	echo "[+] But you can download driver! Enter: 1"
@@ -64,6 +12,67 @@ if [[ $(uname -o) != "GNU/Linux" ]]; then
 		exit
 	fi
 fi
+
+function install_necessary(){
+	sudo apt -y install git
+}
+
+function show_list(){
+	echo "0. Show menu"
+	echo "1. Downdload driver"
+	echo "2. Installing/starting with argument - degresses (default 180)"
+	echo "3. Return to hdmi-mode"
+	echo "4. Delete driver"
+	echo "5. Exit"
+	echo "--- ---"
+}
+
+function get_driver(){
+	install_necessary
+	cd ~
+	if ! [[ -d tools  ]]; then
+		mkdir tools
+		cd tools
+	fi
+	
+	git clone https://github.com/waveshare/LCD-show.git
+	chmod +x LCD-show/LCD28-show
+	echo "[+] Successfully downloaded!"
+	echo "--- ---"
+}
+
+function driver_start(){
+	cd ~/tools/LCD-show/
+	echo "[?] Are you sure? y/n"
+	read yes_or_no
+	if [[ $yes_or_no == "y" ]];then
+		echo "[?] Enter degreeses:"
+		read degreeses
+		./LCD28-show $degreeses
+		echo "[+] Bam-bam! Restart!"
+	else
+		echo "[-] Exitting!"
+		exit
+	fi
+}
+
+function driver_stop(){
+	cd ~/tools/LCD-show/
+	./LCD-hdmi
+	echo "[+] Tyk-tyk! Restart!"
+}
+
+function delete_driver(){
+	echo "Are you sure? y/n"
+	read yes_or_no
+	if [[ $yes_or_no == "y" ]];then
+		cd ~/tools/
+		sudo rm -r LCD-show
+		echo "[+] Successfully deleted!"
+		echo "--- ---"
+	else
+		echo "[-] Exitting!"
+}
 
 show_list
 
@@ -81,7 +90,9 @@ do
 	elif [[ $action -eq 3 ]]; then
 		driver_stop
 		break
-	elif [[ $action -eq 4 ]]; then
+	elif [[ $action -eq 5 ]]; then
+		delete_driver
+	elif [[ $action -eq 5 ]]; then
 		exit
 	else
 		echo "[-] Try again!"
